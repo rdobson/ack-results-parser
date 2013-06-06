2563a0b879d04f20e73aeecf50567a808ddc0c55
Sagnik Datta <sagnik.datta@citrix.com>
2013-06-06 19:43:21 +0530

    Includes 1) Script to download attachment from tracker 2) Push to FTP repo 3) Remote Copy to Project CRD 4) Added existing code: acklogparser + ackFolderParser

---
+++ ackTrackerPlugin.py
@@ -0,0 +1,222 @@
+#!/usr/bin/env python                                                     
+                                                                          
+from jira.client import JIRA, GreenHopper                                 
+#from creds import *                                                       
+import tempfile, tarfile, os
+from acklogparser import *
+from ftpclient import *
+import ftplib, netrc
+from argparse import ArgumentParser                                                                          
+server_url = 'https://tracker.vmd.citrix.com'                                                         
+                                                                          
+jira = JIRA(options={'server':server_url},                                
+            #basic_auth=(tracker_username, tracker_password),              
+           )                                                              
+                                                                          
+class JiraTicket(object):                                                 
+                                                                          
+    def __init__(self, jira, ticket_id):                                  
+        self.jira = jira                                                  
+        self.tid = ticket_id  
+        self.issue = jira.issue(ticket_id, expand="attachment")           
+        self.key = self.issue.key
+        self.validate()                                                   
+                                                                          
+    def validate(self):                                                   
+        pass                                                              
+                                                                          
+    def get_type(self):                                                   
+        return self.issue.fields.issuetype.__dict__['name']               
+                                                                          
+    def get_field(self, name):                                            
+        return getattr(self.issue.fields, name)                           
+                                                                          
+    def get_summary(self):                                                
+        return self.get_field('summary')                                  
+                                                                          
+    def get_description(self):                                            
+        return self.get_field('description')                              
+    
+    def get_attachment_object(self, id):
+        return jira.attachment(id)                                                                      
+
+    def get_attachment_path(self, id):                                            
+        """Returns attachment file path"""
+        att_obj = self.get_attachment_object(id)
+        url = att_obj.raw['content']
+        (fh, attachment_path) = tempfile.mkstemp()
+        os.close(fh)
+        os.system("curl -n %s -o %s -s" % (url,attachment_path))
+        return attachment_path
+    
+    def get_attachmentzip_path(self, id):
+        """Returns attachment file path"""
+        url = "%s/secure/attachmentzip/%s.zip" % (server_url, id)
+        (fh, zippath) = tempfile.mkstemp()
+        os.close(fh)
+        os.system("curl -n %s -o %s -s" % (url,zippath))           
+        return zippath
+    
+    def create_issue_link(self, remote_key):
+        """:param remote_key is key of the remote ticket to be linked"""
+        return self.jira.create_issue_link('Related',self.key,remote_key)                                                                      
+
+    def add_comment(self, comment):
+        return self.jira.add_comment(self.key, comment)
+
+    def create_issue(self, field_dict):
+        return self.jira.create_issue(fields= field_dict)
+
+    def assign_issue(self, user):
+        print (user)
+        return self.jira.assign_issue(self.key, user)
+
+    def list_comments(self): 
+        return self.jira.comments(self.key)
+
+    def add_attachment(self, filepath, filename):
+        return (self.jira.add_attachment(self.issue, filepath, filename))
+        
+class EpicTicket(JiraTicket):                                             
+                                                                          
+    def get_epic_name(self):                                              
+        epic_name_field_id = 'customfield_11337'                          
+        return self.issue.__dict__['raw']['fields'][epic_name_field_id]   
+                                                                          
+                                                                          
+class HCLSubmission(JiraTicket):                                          
+    def validate(self):                                                   
+        if self.get_type() != 'HCL Submission':                           
+            raise Exception("Not a HCL Submission! (%s)" % self.get_type())
+    
+    def get_ack_attachment(self):
+        """Returns tuple of (ack_path, ack_filename)"""
+        for file in self.issue.fields.attachment:
+            if 'ack-submission' in att_obj.filename:
+                return (self.get_attachment_path(file.id), file.filename)
+        print ("Error: ACK Submission Log is missing in this HCL Submission")
+        return (None,None)
+    
+    def get_ack_attachment_dict(self, att_path):
+        """if type ==Server, Prints dict and returns Dict"""
+        #TODO Add type check
+        result_dict = result_parser(att_path,os.getcwd())
+        #TODO Remove printing Dict here :
+        display_results(result_dict)
+        return result_dict
+
+    def get_sub_type(self):
+        """Classify the submission into Server|Storage|NIC|HBA|CNA|GPU|DD"""
+        #TODO
+        
+        pass
+
+    def get_device_tested(self):
+        """Derives name from Device Tested Column"""
+        return self.issue.fields.customfield_10132
+
+
+class DDSubmission(HCLSubmission):
+    def validate(self):
+        if self.get_type() != 'Driver Disk Submission':
+            raise Exception("Not a DD Submission! (%s)" % self.get_type())
+
+class Task(HCLSubmission): 
+    def validate(self):
+        if self.get_type() != 'Task':
+            raise Exception("Not a Task! (%s)" % self.get_type())           
+
+class GenericSubmission(HCLSubmission):
+    def validate(self):
+        pass
+
+class RemoteCopyToCRD():
+    """Create remote copy in Project CRD of given ticket for update marketplace"""
+    #TODO
+    crd_user = 'sagnikd'
+    crd_pjt_key = 'HCL'
+
+    def add_hcl_link_comment(self, master_ticket, crd_ticket):
+        for comment in master_ticket.list_comments():
+            if re.search("http://", comment.body):
+                crd_ticket.add_comment(comment.body)
+
+    def get_doc_attachment(self, master_ticket):
+        for file in master_ticket.issue.fields.attachment:
+            if re.search ("msword",file.mimeType):
+                return (master_ticket.get_attachment_path(file.id), file.filename)
+        print("Error: Missing the Verification Form (doc) which is needed to update Citrix Ready for market place.")
+        return (None, None)
+                         
+    def run(self, master_ticket):
+        
+        (filepath, filename) = self.get_doc_attachment(master_ticket)
+        if filepath:
+            t = master_ticket.create_issue( 
+                        {'project'  : {'key': self.crd_pjt_key},
+                         'summary'   : 'CLONE Of %s' % master_ticket.get_summary(),
+                         'issuetype' : {'name':'Task'},           
+                         'description': 'Please add the device to marketplace'
+                         })
+        
+        self.crd_ticket = Task(jira,t.key)
+        self.crd_ticket.create_issue_link(master_ticket.key)
+        
+        self.add_hcl_link_comment(master_ticket, self.crd_ticket)
+        self.crd_ticket.add_attachment(filepath, filename)
+        self.crd_ticket.add_comment("Hi Gaurav,\nCould you please update this to market place and attach the link.\nThanks,\nSagnik")
+        self.crd_ticket.assign_issue(self.crd_user)
+        return self.crd_ticket
+
+
+
+            
+    
+def main(options):
+    #Dictionary which maps the Folder directory with the type 
+    tag_dict = {'server':'Servers','stor':'Storage Arrays','nic': 'NICs' ,'hba':'HBAs and CNAs', 'cna':'HBAs and CNAs', 'gpu':'GPUs', 'dd':'Driver Disks', 'test':'blaj'}
+    
+    key = options.key.split(',')[1]
+    if tag_dict[key] == 'server':
+        ticket = HCLSubmission(jira, options.key.split(',')[0])
+    else:
+        ticket = GenericSubmission(jira, options.key.split(',')[0])
+    print ticket.get_summary()
+
+    #For non HCL Submission, we need additional parameters as below
+    if len(options.key.split(','))> 2:
+        product_name = options.key.split(',')[3]
+        version = options.key.split(',')[2]        
+    
+    #To display the ack-submission if there is one:
+    if ticket.get_type() == 'HCL Submission':
+        (ack_path, ack_filename) = ticket.get_ack_attachment()
+        print ("%s found.\nExtracting Product Info.." % ack_filename)
+        dict = ticket.get_ack_attachment_dict(ack_path)
+        version = dict['xs_version']
+
+        product_name = ticket.get_device_tested()  #override product name with "Device Tested" field
+        #if Device Tested is empty, product name will be taken from result dict
+        if not product_name:
+            product_name = "%s %s" % (dict['system-manufacturer'].strip(), 
+                                      dict['product'].strip())
+    print ("\nDevice Tested: %s" % product_name)
+    
+    session = FTPClient().traverse_to_upload_path(version, tag_dict[key], product_name)
+    #Path of zipfile that will be stored
+    zippath = ticket.get_attachmentzip_path(ticket.issue.id)
+    zipfile = ticket.issue.key + ".zip"
+    FTPClient().upload_file(session, zippath, zipfile)
+    
+    #RemoteCopy to CRD if required. 
+    t2 = RemoteCopyToCRD().run(ticket)
+    if t2 is not None:
+        print ("%s Created" % t2.key)
+        print (t2.get_summary())
+
+if __name__ == "__main__":
+     parser = ArgumentParser()
+     parser.add_argument("-t", "--ticket", dest="key",required=True,help="HCL-435,(server|stor|nic|hba|cna|gpu|dd), 6.2.0[Optional], Product_Name [Optional]")
+     args = parser.parse_args() 
+     main(args)
+
---
+++ acklogparser.py
@@ -0,0 +1,179 @@
+#!/usr/bin/python
+nics_dict ={}
+hbas_dict ={}
+machine_dict={"pass":[],"fail":[]} #seprates server Products PASS/FAIL
+failed_dict={}
+dict = {'xs_version':u'None','system-manufacturer':'None','sockets':'None','product':'None',"chassis":'None','modelname':'None','family':'None','model':'None','stepping':'None','nics':[], 'hbas':[]}
+
+from argparse import ArgumentParser
+import tarfile,os,re,xml.dom.minidom,pprint
+#import models
+
+def extract_file_from_tar(tarfilename, filename, dest):
+    tf = tarfile.open(tarfilename) #tarfilename is path of tar
+    path = filter(lambda x:os.path.basename(x) == filename, tf.getnames())
+    if not len(path):
+        path = filter(lambda x:re.search(filename, x),tf.getnames())        
+    map(lambda x:tf.extract(x, path=dest), path)
+    return os.path.join(dest,path[0])
+
+def result_parser(tarfilename, logsubdir):
+    bugtools_path = extract_file_from_tar(tarfilename = tarfilename, filename = "bug-report", dest = logsubdir)
+    testconf_path = extract_file_from_tar (tarfilename, 'test_run.conf', logsubdir)
+    dmidecode_path = extract_file_from_tar (os.path.join(logsubdir, bugtools_path), 'dmidecode.out', logsubdir)
+    #xapi_db_path = extract_file_from_tar (os.path.join(logsubdir, bugtools_path), 'xapi-db.xml', logsubdir)
+    lspcivv_path = extract_file_from_tar (os.path.join(logsubdir, bugtools_path), 'lspci-vv.out', logsubdir)
+    #get NIC Names, CPU
+    test_conf = xml.dom.minidom.parse(open(testconf_path))
+    
+    #XS-version
+    for version in test_conf.getElementsByTagName("global_config"):
+        if version._attrs.has_key('xs_version'):
+            dict['xs_version'] = version._attrs['xs_version'].nodeValue
+
+    #CPU info and HBA pci-id info
+    hbaBusIdList=[]
+    for device in test_conf.getElementsByTagName("device"):
+        if device._attrs.has_key('family'):
+            dict['family'] = device._attrs['family'].nodeValue
+        if device._attrs.has_key('stepping'):
+            dict['stepping'] = device._attrs['stepping'].nodeValue
+        if device._attrs.has_key('model'):
+            dict['model']= device._attrs['model'].nodeValue
+        if device._attrs.has_key('modelname'):
+            dict['modelname']= device._attrs['modelname'].nodeValue
+        if device._attrs.has_key('socket_count'):
+            dict['sockets'] = device._attrs['socket_count'].nodeValue
+        if device._attrs.has_key('PCI_description'):
+            if device._attrs['PCI_description'].nodeValue not in dict['nics']:
+                dict['nics'].append(device._attrs['PCI_description'].nodeValue)
+        if device._attrs.has_key('device'):
+            hbaBusIdList.append(device._attrs['id'].nodeValue)
+            
+            
+    #Chassis used info                 i
+    lines = open(dmidecode_path).readlines()
+    
+    for i in range(len(lines)):
+        if re.search('Chassis Information',lines[i]):
+            for j in range(len(lines[i:])):
+                if re.search("Type", lines[i+j]):
+                    list = re.findall('(\w+):([\w\s\-]+)',lines[i+j])[0]
+                    #print "%s" % list
+                    dict['chassis']= list[1]
+                    break          
+            break       
+    #vendor name
+    for i in range(len(lines)):
+        if re.search("System Information", lines[i]):
+            for j in range(len(lines[i:])):
+                if re.search("Manufacturer", lines[j+i]):
+                    list = re.findall('([\w\s]+):([\w\s\-\[\]\.]+)',lines[i+j])[0]
+                    dict['system-manufacturer'] = list[1]
+                    break
+            break
+    #Pdt name                
+    for i in range(len(lines)):    
+        if re.search("System Information", lines[i]):
+            for j in range(len(lines[i:])):
+                if re.search("Product Name", lines[j+i]):
+                    list = re.findall('([\w\s]+):([\w\s\-\[\]\.^\n]+)',lines[i+j])[0]
+                    dict['product']= list[1]
+                    break
+            break     
+    #TODO ONLY IF format of logs are from ackdownload.py, "machine" fetches exists
+    machine = tarfilename.split("-ack")[0]    
+    
+    #lcpci -vv lines for extracting HBA data(Note: This is a workaround due to existing bug of ACK not catching the right hba pci-ids)
+    lines = open(lspcivv_path).readlines()
+    
+    #HBAs - Storage Controllers
+    string_pattern = ['SCSI storage controller', 'SATA controller', 'RAID bus controller']
+    index = []
+    for i in range(len(lines)):
+        for string in string_pattern:
+            if re.search(string, lines[i]):
+                index.append(i)
+    for i in index:
+        dict['hbas'].append(re.findall('.*: ([\w\s\-\(\)\/\[\]]+)',lines[i])[0].strip())
+    
+    return dict
+
+def display_results(dict, keys = None):
+    #Display rests
+    keys = ['xs_version','system-manufacturer','product','sockets','chassis','modelname','family','model','stepping','nics','hbas']
+    if keys is None:
+        keys = dict.keys()
+    for key in keys:
+        if type(dict[key]) =='list':
+            print "%50s :" % key, "%-50s" % pprint.pprint(dict[key])
+        else:
+            print "%50s : %s" % (key, dict[key])
+
+def countTestFailures(tarfilename):
+    testconf_path = extract_file_from_tar(tarfilename, 'test_run.conf', os.getcwd())
+    test_conf = xml.dom.minidom.parse(open(testconf_path))
+    count = 0
+    for result in test_conf.getElementsByTagName('result'):
+        if result.firstChild.nodeValue != 'pass':
+            count = count + 1
+    #fail product lists
+    return count       
+    
+
+def main(options):
+    tarfilename = options.filename #downloadACK(runjob, runmachine)
+    xenrtmachine=None #runmachine
+    dict = result_parser(tarfilename,os.getcwd())
+    #keys = ['system-manufacturer','product','chassis','sockets','family','model','stepping','nics', 'hbas']
+    #print dict    
+    #display_results(dict, keys)
+    display_results(dict)
+    
+    count = countTestFailures(tarfilename)
+    testconf_path = extract_file_from_tar(tarfilename, 'test_run.conf', os.getcwd())
+    test_conf = xml.dom.minidom.parse(open(testconf_path))
+
+    #fail product lists
+    if count > 0 :
+        exception_list=[]
+        for exception in test_conf.getElementsByTagName('exception'):
+            if exception.firstChild.nodeValue not in exception_list:
+                exception_list.append(exception.firstChild.nodeValue)
+        #maintain machine_dict only for xenRT machines
+        if xenrtmachine:
+            failed_dict[xenrtmachine]= exception_list
+
+            if dict['product'] not in machine_dict['pass']:
+                if dict['product'] not in machine_dict['fail']:    
+                    machine_dict['fail'].append(dict['product'])
+            print ("*******%s tests FAILED for %s *********" % (count,runmachine))
+        else:
+            failed_dict[dict['product']] = exception_list
+    else:
+        if xenrtmachine and dict['product'] not in passed_list : #added check here
+            if dict['product'] not in machine_dict['pass']: #remove duplicacy 
+                machine_dict['pass'].append(dict['product'])
+    if xenrtmachine:
+        print ("#"*30)
+        print ("NICS LISTING HERE")
+        display_results(nics_dict)
+        print ("NICS LISTING OVER")
+        print ("#"*30)
+        print ("HBAs HERE")
+        display_results(hbas_dict)
+        print ("HBAs listing over")
+        print ("#"*30)
+        print ("MY PASSED PRODUCTS")
+        pprint.pprint(machine_dict['pass'])
+        print ("#"*30)
+        print ("MY FAIL PRODUCTS")
+        pprint.pprint(machine_dict['fail'])
+        print ("#"*30,"failed_dict below")
+    display_results(failed_dict)
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--file", dest="filename",required=True,help="ACK tar file")
+    args = parser.parse_args()
+    main(args)

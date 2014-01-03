2deaca9aad43d501460e396c9075eef2f44de59c
Sagnik Datta <sagnikd@jarvis.uk.xensource.com>
2014-01-03 07:20:29 +0000

    merging back the FTP Class

--- ackTrackerPlugin.py
+++ ackTrackerPlugin.py
@@ -4,7 +4,6 @@ from jira.client import JIRA, GreenHopper
 #from creds import *                                                       
 import tempfile, tarfile, os
 from acklogparser import *
-from ftpclient import *
 import ftplib, netrc
 from argparse import ArgumentParser                                                                          
 server_url = 'https://tracker.vmd.citrix.com'                                                         
@@ -92,7 +91,7 @@ class HCLSubmission(JiraTicket):
     def get_ack_attachment(self):
         """Returns tuple of (ack_path, ack_filename)"""
         for file in self.issue.fields.attachment:
-            if 'ack-submission' in att_obj.filename:
+            if 'ack-submission' in file.filename:
                 return (self.get_attachment_path(file.id), file.filename)
         print ("Error: ACK Submission Log is missing in this HCL Submission")
         return (None,None)
@@ -169,6 +168,60 @@ class RemoteCopyToCRD():
         return self.crd_ticket
 
 
+class SFFTPClient():
+
+    def __init__(self):
+        self.machine = 'citrix.sharefileftp.com'
+
+    def get_session(self, machine):
+        info = netrc.netrc("/home/sagnikd/.netrc")
+        if info.authenticators(machine):
+            (user, account, password)= info.authenticators(machine)
+        else:
+            print("User credentials not present in .netrc for %s" % machine)
+        return ftplib.FTP(machine,user,password)
+
+    def get_ticket_type(self, attachment):
+        """Determines and returns the type of submission = 'Servers'|'Storage Arrays'|'NICs'|'HBAs and CNAs'|'GPUs'|'Driver Disks'"""
+        pass
+
+    def traverse_to_upload_path(self, version, ticket_type, filename):
+        """Returns the session created after traversing to appropriate upload path for the required XS version and ticket type"""
+        session = self.get_session(self.machine)
+        #Logs are stored in /XenServer HCL/Hardware Certification Logs/XenServer *
+        try:
+            session.cwd('XenServer HCL')
+            session.cwd('Hardware Certification Logs')
+            for folder in session.nlst():
+                if re.search(version, folder):
+                    session.cwd(folder)
+                    session.cwd(ticket_type)
+
+                    #Note filename is the product name
+                    #filename =  "%s %s" % (manufacturer, product)
+                    print ("\n")
+                    if filename not in session.nlst():
+                        session.mkd(filename)
+                    else:
+                        print ("Submission found to be already existing. Adding these logs in the same folder.")
+                    session.cwd(filename)
+                    print ("Successfully traversed to upload path: %s" % session.pwd())
+                    return session
+            print("Could not find the version at all")
+        except:
+            print ("FTP Directory doesn't exist. Must ahve been moved.")
+            session.close()
+
+    def upload_file(self, session, upload_filepath, filename):
+        """Uploads file to current directory on sharefile"""
+        print ('Uploading file %s' % filename)
+        print ('Upload to directory %s' % session.pwd())
+        out = session.storbinary('STOR %s' % filename ,open(upload_filepath, 'rb'), blocksize = 8192*1024)
+        if re.search('Transfer Complete', out):
+            print ("Transfer Completed.")
+            return
+        else:
+            print ("Transfer was unsuccessful. Please check file size")
 
             
     
@@ -202,11 +255,11 @@ def main(options):
                                       dict['product'].strip())
     print ("\nDevice Tested: %s" % product_name)
     
-    session = FTPClient().traverse_to_upload_path(version, tag_dict[key], product_name)
+    session = SFFTPClient().traverse_to_upload_path(version, tag_dict[key], product_name)
     #Path of zipfile that will be stored
     zippath = ticket.get_attachmentzip_path(ticket.issue.id)
     zipfile = ticket.issue.key + ".zip"
-    FTPClient().upload_file(session, zippath, zipfile)
+    SFFTPClient().upload_file(session, zippath, zipfile)
     
     #RemoteCopy to CRD if required. 
     t2 = RemoteCopyToCRD().run(ticket)
